20a2eb07323811f6b5daf76a8d0dd4e091ac4dd0
TheDrakonZX <49098905+TheDrakonZX@users.noreply.github.com>
2019-08-16 16:52:32 -0300

    Conectando Banco

---
+++ index.php
@@ -0,0 +1,28 @@
+<?php
+    $dsn = "mysql:dbname=index;host=127.0.0.1";
+    $dbuser = "root";
+    $dbpass = "";
+    
+
+    try
+    {
+        $pdo = new PDO($dsn,$dbuser,$dbpass);
+        echo "Conexão concluido com sucesso !  ";
+        
+        $sql = "SELECT * FROM posts";
+        $dado = $pdo->query($sql);
+        
+        if ($dado->rowCount() > 0)
+        {
+            echo "Há posts cadastrados";
+        }
+        else
+        {
+            echo "Não há posts cadastrados";
+        }
+    }
+    catch(PDOException $e)
+    {
+        echo "Conexão falhou :(".$e->getMessage();
+    }
+?>
